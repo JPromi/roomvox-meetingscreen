e4e64a40ddc20ea3d04d58e79a589b348a1532c5
JPromi <jonasmpr05@gmail.com>
2026-08-17 18:44:34 +0200

    Set init size set minimum size

--- desktopApp/bin/main/com/jpromi/room_vox/meetingscreen/main.kt
+++ desktopApp/bin/main/com/jpromi/room_vox/meetingscreen/main.kt
@@ -1,13 +1,19 @@
 package com.jpromi.room_vox.meetingscreen
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "RoomVox Meetingscreen",
+        resizable = true,
+        state = WindowState(width = 1000.dp, height = 600.dp)
     ) {
+        window.minimumSize = Dimension(800, 600)
         App()
     }
 }
